0efd76edefaad4932939e8034705e955991bbb04
Nam Le <NamPhgLe>
2025-06-24 21:03:44 -0700

    added navbar + routing

--- game-simulator/src/App.tsx
+++ game-simulator/src/App.tsx
@@ -1,17 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/Navbar';
 import ItemFetcher from './components/lol-item-fetch';
 import ChampionFetcher from './components/lol-champ-fetch';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
 
-function App() {
+function Home() {
   return (
-    <div>
-      <LoginForm />
-      <SignupForm />
+    <>
       <h1>Game Calculator</h1>
       <ItemFetcher />
       <ChampionFetcher />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </>
   );
 }
 
